0169426b30550679285be2936aed9bf366efedb9
Mao Han <han_mao@c-sky.com>
2018-04-08 14:39:09 +0800

    C-SKY: Linux ABI
    Linux-specific code that is required for maintaining ABI compatibility.
    This doesn't contain the actual system call interface.
    
            * sysdeps/csky/nptl/pthread-offsets.h: New file
            * sysdeps/csky/nptl/pthreaddef.h: New file
            * sysdeps/unix/sysv/linux/csky/Versions: New file
            * sysdeps/unix/sysv/linux/csky/bits/fcntl.h: New file
            * sysdeps/unix/sysv/linux/csky/bits/mman.h: New file
            * sysdeps/unix/sysv/linux/csky/bits/shm.h: New file
            * sysdeps/unix/sysv/linux/csky/c++-types.data: New file
            * sysdeps/unix/sysv/linux/csky/csky_readtp.h: New file
            * sysdeps/unix/sysv/linux/csky/ipc_priv.h: New file
            * sysdeps/unix/sysv/linux/csky/jmp_buf-macros.h: New file
            * sysdeps/unix/sysv/linux/csky/kernel-features.h: New file
            * sysdeps/unix/sysv/linux/csky/makecontext.c: New file
            * sysdeps/unix/sysv/linux/csky/profil-counter.h: New file
            * sysdeps/unix/sysv/linux/csky/register-dump.h: New file
            * sysdeps/unix/sysv/linux/csky/setcontext.S: New file
            * sysdeps/unix/sysv/linux/csky/shlib-versions: New file
            * sysdeps/unix/sysv/linux/csky/sigcontextinfo.h: New file
            * sysdeps/unix/sysv/linux/csky/swapcontext.S: New file
            * sysdeps/unix/sysv/linux/csky/sys/cachectl.h: New file
            * sysdeps/unix/sysv/linux/csky/sys/procfs.h: New file
            * sysdeps/unix/sysv/linux/csky/sys/ucontext.h: New file
            * sysdeps/unix/sysv/linux/csky/sys/user.h: New file
            * sysdeps/unix/sysv/linux/csky/sysdep-cancel.h: New file
            * sysdeps/unix/sysv/linux/csky/sysdep.S: New file
            * sysdeps/unix/sysv/linux/csky/sysdep.h: New file
            * sysdeps/unix/sysv/linux/csky/ucontext_i.sym: New file

---
+++ sysdeps/unix/sysv/linux/csky/sigcontextinfo.h
@@ -0,0 +1,37 @@
+/* Copyright (C) 2018 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library.  If not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#define SIGCONTEXT siginfo_t *_si, struct ucontext *
+#define SIGCONTEXT_EXTRA_ARGS _si,
+#define GET_PC(ctx)	((void *) (ctx)->uc_mcontext.sc_pc)
+/* r8 is fp, make sure it is in the same area in sigcontext.  */
+#define GET_FRAME(ctx)	((void *) (ctx)->uc_mcontext.sc_regs[2])
+#define GET_STACK(ctx)	((void *) (ctx)->uc_mcontext.sc_usp)
+#define CALL_SIGHANDLER(handler, signo, ctx) \
+  (handler)((signo), SIGCONTEXT_EXTRA_ARGS (ctx))
+
+/* There is no reliable way to get the sigcontext unless we use a
+   three-argument signal handler.  */
+#define __sigaction(sig, act, oact) ({ \
+  (act)->sa_flags |= SA_SIGINFO; \
+  (__sigaction) (sig, act, oact); \
+})
+
+#define sigaction(sig, act, oact) ({ \
+  (act)->sa_flags |= SA_SIGINFO; \
+  (sigaction) (sig, act, oact); \
+})
